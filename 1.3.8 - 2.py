26893757f9d208f79a370b7048cc9e6c8bd35ca7
Larsoluk000 <60366481+Larsoluk000@users.noreply.github.com>
2020-01-29 14:47:47 -0600

    Add files via upload

---
+++ 1.3.8 - 2.py
@@ -0,0 +1,17 @@
+
+import random
+def goguess():
+    num_guesses = 0
+print ('I have a number between 1 and 20 inclusive')
+secret_number = random.randint(1, 20)
+while guess != secret_number:
+    if secret_number != goguesses:
+        num_guess += 1
+    print ("Wrong, guess again")
+        if secret_number < goguess:
+            print (goguess + 'is to low')
+            if secret_number > goguess:
+                print (goguess + 'is to high')
+                else:
+                    print ('My number is equal to' + goguess +'You guessed in '
+                
